2e2927a6b294bc7743718e4d7f4453161423eb56
Lucas Menezes <luckmenez@gmail.com>
2021-03-11 22:05:19 -0300

    criado base de transaction list e transaction form

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'transaction_form.dart';
+import 'transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 300.21,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Sapato social',
+      value: 250.11,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Expenses App"),
+          elevation: 5,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.red,
+                child: Text("Gráfico"),
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+    );
+  }
+}
